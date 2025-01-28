22e79a186f5d95789036f4a2dc90676eb69e9113
ashtonbryant <ashton.bryant@temple.edu>
2025-01-27 22:11:24 -0500

    test website thats able to receive and send data

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def index_func():
+
+    data = ''
+    # check for request data through GET method
+    if request.method == 'GET':
+
+        # evalate the choices 
+        genre = request.args.get('genre')
+        if      genre == 'comedy':
+            data = 'SEND COMEDY LIST'
+
+        elif    genre == 'drama':
+            data = 'SEND DRAMA LIST'
+            
+        elif    genre == 'action':
+            data = 'SEND ACTION LIST'
+
+        return render_template("index.html",type = data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
